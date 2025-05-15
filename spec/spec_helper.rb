f8a04a8e24c23d36acf323f66fe20761b7f35e47
Thomas Vincent <thomasvincent@gmail.com>
2025-05-14 17:00:08 -0700

    chore: remove berkshelf and legacy references

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,7 @@
 require 'chefspec'
-require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
-  # Specify the Chef log level (default: :warn)
+  config.color = true
+  config.formatter = :documentation
   config.log_level = :error
-
-  # Specify the operating platform to mock Ohai data from
-  config.platform = 'ubuntu'
-
-  # Specify the operating version to mock Ohai data from
-  config.version = '20.04'
 end
